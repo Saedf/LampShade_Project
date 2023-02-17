cb7aad334945ee1c2d0923a2b86c47ae49e62f60
Saed Fathi <saed.fathi@gmail.com>
2023-02-17 22:34:16 +0330

    AddModulesToCollectionService

---
+++ LampShade/ShopManagement.Infrastructure.Core/ShopManagementBoostrapper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ShopManagement.Application;
+using ShopManagement.Application.Contract.ProductCategory;
+using ShopManagement.Domain.ProductCategoryAgg;
+using ShopManagement.Infrastructure.EfCore;
+using ShopManagement.Infrastructure.EfCore.Repository;
+
+namespace ShopManagement.Infrastructure.Core
+{
+    public class ShopManagementBoostrapper
+    {
+        public static void Configure(IServiceCollection services, string? connectionString)
+        {
+            services.AddDbContext<ShopContext>(x=>x.UseSqlServer(connectionString));
+
+            services.AddTransient<IProductCategoryRepository, ProductCategoryRepository>();
+            services.AddTransient<IProductCategoryApplication,ProductCategoryApplication>()
+        }
+
+    }
+}
