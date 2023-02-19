0f60a63881652778b39363d51e10eb1c8feb811c
Saed Fathi <saed.fathi@gmail.com>
2023-02-19 07:44:14 +0330

    CreateUIOfSearchModelForListProductCategory

--- LampShade/ServiceHost/Areas/Administration/Pages/Shop/ProductCategories/Index.cshtml.cs
+++ LampShade/ServiceHost/Areas/Administration/Pages/Shop/ProductCategories/Index.cshtml.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ShopManagement.Application.Contract.ProductCategory;
 
 namespace ServiceHost.Areas.Administration.Pages.Shop.ProductCategories
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly IProductCategoryApplication _productCategoryApplication;
+        public List<ViewModelProductCategory> _productCategories;
+        public SearchModelProductCategory SearchModel;
+        public IndexModel(IProductCategoryApplication productCategoryApplication)
         {
+            _productCategoryApplication = productCategoryApplication;
+            //_productCategories=new List<ViewModelProductCategory>();
+        }
+
+        public void OnGet(SearchModelProductCategory searchModel)
+        {
+           _productCategories=_productCategoryApplication.Search(searchModel);
         }
     }
 }
