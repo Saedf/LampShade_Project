55384fe4f322d087b11068502040ff1c53d74488
Saed Fathi <saed.fathi@gmail.com>
2023-03-08 21:00:51 +0330

    ArticleCategoryUIAddedToPageAreaOfAdmin

--- LampShade/BlogManagement.Application.Contracts/ArticleCategory/CreateArticleCategory.cs
+++ LampShade/BlogManagement.Application.Contracts/ArticleCategory/CreateArticleCategory.cs
@@ -1,25 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
+using _01_Framework.Application;
 using Microsoft.AspNetCore.Http;
 
 namespace BlogManagement.Application.Contracts.ArticleCategory
 {
     public class CreateArticleCategory
     {
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Name { get; set; }
         public IFormFile Picture { get; set; }
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string? PictureAlt { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string? PictureTitle { get; set; }
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string? Description { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public int ShowOrder { get; set; }
+
         public string Slug { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string KeyWords { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string MetaDescription { get; set; }
+
         public string? CanonicalAddress { get; set; }
 
     }
